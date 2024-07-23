0bce3fed890bc921ba88e222d2c9549a23045159
majidH <majid.hadadian@gmail.com>
2024-07-23 17:09:26 +0330

    Complete about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,26 @@
 function About() {
   return (
-    <h1 className="text-4xl">About</h1>
-  )
+    <>
+      <div className="flex justify-center flex-wrap items-center gap-3 sm:gap-x-6">
+        <h2 className="text-4xl tracking-tight leading-none sm:text-6xl font-bold  ">
+          We love
+        </h2>
+        <div className="stats bg-primary shadow">
+          <div className="stat ">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-wider ">
+              Comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="text-lg  mt-6 leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, ut!
+        Fugiat non expedita corrupti mollitia corporis, veniam eius ab in
+        reiciendis quisquam aspernatur aliquid repudiandae, sint at sed qui
+        suscipit porro nemo quaerat! Officia reiciendis, sint quis quod
+        praesentium deserunt.
+      </p>
+    </>
+  );
 }
-export default About
+export default About;
